3da059f0c003da491995599b38c72aee7388d623
soulproprietor <alistersdsu@yahoo.com>
2021-06-05 20:01:06 -0700

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("portfolio_app/", include("portfolio_app.urls")),
     path("", include("portfolio_app.urls")),
 ]
